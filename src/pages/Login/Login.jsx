afbfda6eb6c8c1352e10c4896bc0c9efcacc3578
Dustinouwu <dastin.chavez@epn.edu.ec>
2022-12-12 10:00:06 -0500

    register

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'; 
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonInput } from '@ionic/react';
+import { IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonInput } from '@ionic/react';
 
 
 function Login() {
